af04a6840b8e89d935e842c109a6c72062382372
LeNico44 <nicolas.houisse@epsi.fr>
2018-11-05 21:12:11 +0100

    first step R&D !!

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>R&D</title>
+
+    <link rel="stylesheet" href="">
+</head>
+<body>
+<div class="container">
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+    </main>
+    <footer>
+    </footer>
+</div>
+</body>
+</html>
